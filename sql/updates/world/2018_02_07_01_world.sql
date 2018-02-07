eb4e8678eecacaa157267e459cd4c5bb180c9b77
kusakabe <miroshserega@inbox.ru>
2018-02-07 13:53:43 +0400

    База/Существа: Исправлено местоположение укротителя грифонов Стефаноса (17555)

---
+++ sql/updates/world/2018_02_07_01_world.sql
@@ -0,0 +1,4 @@
+-- Stephanos <Hippogryph Master> (17555)
+SET @STEPHANOS := 17555;
+
+UPDATE `creature` SET `position_x` = -3865.313, `position_y` = -11642.25, `position_z` = -137.5662, `orientation` = 2.303835, `spawntimesecs` = 120 WHERE `id` = @STEPHANOS;
